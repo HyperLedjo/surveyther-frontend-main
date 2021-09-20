a9e61a84286a8fb9bca203c1655f097f1aa328da
KimH4nKyul <alskim93@kakao.com>
2021-09-20 19:48:26 +0900

    update config

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Landing from './pages/Landing.vue';
 import Login from './pages/Login.vue';
 import Profile from './pages/Profile.vue';
 import MainNavbar from './layout/MainNavbar.vue';
-import MainFooter from './layout/MainFooter.vue';
+// import MainFooter from './layout/MainFooter.vue';
 
 import Header from './layout/Header.vue';
 import Footer from './layout/Footer.vue';
@@ -14,6 +14,7 @@ import Main from './pages/main.vue';
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   linkExactActiveClass: 'active',
   routes: [
     {
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  proxy: {
+    'api': {
+      target: 'http://localhost:8082',
+      changeOrigin: true
+    },
+    'login': {
+      target: 'http://localhost:8082',
+      changeOrigin: true
+    },
+    'logout': {
+      target: 'http://localhost:8082',
+      changeOrigin: true
+    }
+  },
   css: {
     // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== 'production'
