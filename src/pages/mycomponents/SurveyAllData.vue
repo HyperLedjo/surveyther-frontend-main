9479e8187b68a4b4c1598dea6f35605b5dad592f
Ys-ng <ys_ng0121@naver.com>
2021-10-17 17:42:49 +0900

    ys category fixed

--- src/pages/mycomponents/SurveyAllData.vue
+++ src/pages/mycomponents/SurveyAllData.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="">
-      {{$store.state.surveyAllData}}--
-          <v-list-tile
+      <!-- {{$store.state.surveyAllData}}-- -->
+          <!-- <v-list-tile
             v-for="(survey) in $store.state.surveyAllData"
             :key="survey"
             >
-            <!-- <template v-if="survey.category == $route.params.category && survey.status == $route.params.status"> -->
                 <a href="/survey/detail">
               <card >
                   <div >
@@ -18,13 +17,10 @@
                               <span class="badge  mr-1"
                                     v-bind:class="{ 'badge-success' : survey.status == '진행중' , 'badge-default' : survey.status == '마감'}"
                                 >{{survey.status}}</span>
-                              <!-- <span class="badge badge-success mr-1">{{survey.status}}</span> -->
                                 <span  v-if="survey.status == '진행중'" class="badge badge-warning">{{survey.paymAmount*0.98*0.4}}---{{rewardPerOne(survey.paymAmount)}}</span>
                                 <span><b> {{survey.title}}</b></span> 
                             </div>
-                            <!-- <div class="col-sm-6 col-lg mt-1">
-                                <b> <h6>서베이 타이틀입니다</h6></b>
-                            </div> -->
+                            
                             <div class="surv-disc col-sm-6 col-lg-12 mt-1">
                                 <h6> {{survey.subtitle}}</h6>
                             <hr style="border: solid 1px rgb(200,200,200);">
@@ -54,7 +50,64 @@
                   </div>
               </card>
             </a>
-            <!-- </template> -->
+          </v-list-tile> -->
+          <!-- ------------------------------------------------------------------ -->
+          <v-list-tile
+            v-for="(survey) in $store.state.surveyAllData"
+            :key="survey"
+            >
+             <!-- v-if="survey.category == $route.params.category" -->
+            <template v-if="survey.category == $route.params.category && survey.status == $route.params.status">
+                <router-link :to="{name: 'survey_detail', params:{surveyId: survey.surveyId}}">
+              <card >
+                  <div >
+
+                    <div id="inputs">
+                        <div class="row card-form">
+
+                            <div class="col-sm-6 col-lg-12">
+                              <span class="badge badge-primary mr-1">{{survey.category}}</span>
+                              <span class="badge  mr-1"
+                                    v-bind:class="{ 'badge-success' : survey.status == '진행중' , 'badge-default' : survey.status == '마감'}"
+                                >{{survey.status}}</span>
+                              <!-- <span class="badge badge-success mr-1">{{survey.status}}</span> -->
+                                <span  v-if="survey.status == '진행중'" class="badge badge-warning">{{survey.paymAmount*0.98*0.4/survey.targetAmount}}</span>
+                                <span><b> {{survey.title}}</b></span> 
+                            </div>
+                            <!-- <div class="col-sm-6 col-lg mt-1">
+                                <b> <h6>서베이 타이틀입니다</h6></b>
+                            </div> -->
+                            <div class="surv-disc col-sm-6 col-lg-12 mt-1">
+                                <h6> {{survey.subtitle}}</h6>
+                                 <!-- style="border: solid 1px rgb(200,200,200);" -->
+                            <hr>
+                            </div>
+                            <div class="col-sm-6 col-lg-5">
+                              <span class="badge badge-neutral mr-1">#태그</span>
+                              <span class="badge badge-neutral mr-1">#서베이</span>
+                              <span class="badge badge-neutral mr-1">#tag</span>
+                              <span class="badge badge-neutral mr-1">#sample</span>
+                            </div>
+                            <div class="surv-disc col-sm-6 col-lg-3">
+                                <div  v-if="survey.status == '진행중'">
+                                    <span class="mr-4">남은보상<b>{{survey.targetAmount-survey.currentAmount}}</b></span>
+                                    <span>마감 <b>{{survey.tillClose}}</b> 일전</span>
+                                </div>
+                            </div>
+                            <div class="surv-disc col-sm-6 col-lg-2">
+                                <span class="ml-3">{{survey.regDate}}</span> 
+                            </div>
+                            <div class="surv-disc col-sm-6 col-lg-2">
+                                <span class="ml-4 now-ui-icons ui-2_chat-round"> {{survey.commentCount}}</span> 
+                                <span class="ml-4 now-ui-icons ui-2_favourite-28"> {{survey.likeCount}}</span> 
+                            </div>
+                        </div>
+                    </div>
+
+                  </div>
+              </card>
+            </router-link>
+            </template>
 
 
 
