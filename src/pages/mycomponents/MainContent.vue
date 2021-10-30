f55197628876b2ee51e81a2605f2e27d4601b670
KimH4nKyul <alskim93@kakao.com>
2021-10-31 01:59:05 +0900

    소개글 작성3

--- src/pages/mycomponents/MainContent.vue
+++ src/pages/mycomponents/MainContent.vue
@@ -8,7 +8,7 @@
                         많은 서베이가 당신의 소중한 의견을 기다리고 있습니다!
                         지금 서베이에 참여하고 더 많은 보상을 받아 가세요! 
                     </p>
-                    <a href="/survey/진행중/부동산"><button class="btn btn-round btn-primary btn-lg">보러가기</button></a>
+                    <a href="/survey/진행중/부동산"><button class="btn btn-round btn-primary btn-lg">참여하러 가기</button></a>
             </div>    
                 
       </div>
@@ -17,9 +17,9 @@
             <div class="left-box img-info">
                 <h4><b>PURCHASE</b></h4>
                 <p>
-                    회사나 연구에 설문조사가 필요한가요? SURVEYTHER에 비슷한 설문조사가 이미 있는지 찾아보고 구매하세요!
+                    회사나 연구에 설문조사가 필요한가요? <i><b>SURVEYTHER</b></i>에 비슷한 설문조사가 이미 있는지 찾아보고 구매하세요!
                 </p>
-                <a href="/survey/마감/부동산"><button class="btn btn-round btn-primary btn-lg">보러가기</button></a>
+                <a href="/survey/마감/부동산"><button class="btn btn-round btn-primary btn-lg">구매하러 가기</button></a>
 
             </div>
             <img class="right-box" src="img/survey_buy.jpeg" alt="" />
@@ -30,9 +30,9 @@
             <div class="right-box">
                     <h4><b> FILL OUT</b></h4>
                     <p>
-                        나만의 서베이를 등록 해보세요! 지불 금액을 원하는 만큼 정하여 지불 할 수 있으며, 지불된 금액은 참여자에게 보상으로 돌아갑니다! 
+                        나만의 서베이를 작성 해보세요! 지불 금액을 원하는 만큼 정하여 지불 할 수 있으며, 지불된 금액은 참여자에게 보상으로 돌아갑니다! 
                     </p>
-                    <a href="/survey_create"><button class="btn btn-round btn-primary btn-lg">보러가기</button></a>
+                    <a href="/survey_create"><button class="btn btn-round btn-primary btn-lg">작성하러 가기</button></a>
 
             </div>    
       </div>
@@ -43,7 +43,7 @@
                 <p>
                     참여후 받은 토큰으로 리워드샵에서 아이스크림, 커피 등 다양한 모바일 상품권으로 교체할수 있어요! 
                 </p>
-                <a href="/rewardshop"><button class="btn btn-round btn-primary btn-lg">보러가기</button></a>
+                <a href="/rewardshop"><button class="btn btn-round btn-primary btn-lg">쇼핑하러 가기</button></a>
 
             </div>
             <img class="right-box" src="img/reward.jpeg" alt="" />
